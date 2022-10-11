9a239235fc82377b8ab73388b81339bbdf52f77e
CristianoAliotta <cristiano.aliotta@outlook.com>
2022-10-11 16:51:19 +0200

    set up

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  ageComparison(person) {
+    if (this.age > person.age) {
+      console.log("${this.name} è più vecchio di ${person.name}");
+    } else {
+      console.log("${this.name} è più giovane di ${person.name}");
+    }
+  }
+}
+
+let p1 = new Person("Tizio", 25);
+let p2 = new Person("Caio", 30);
+let p3 = new Person("Sempronio", 20);
+
+p1.ageComparison(p3);
+p3.ageComparison(p2);
